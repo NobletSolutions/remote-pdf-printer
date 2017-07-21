ff206a1e23c53b8dc3e61c4328231e7ae1064d06
Nathanael d. Noblet <nathanael@noblet.ca>
2017-07-21 16:31:17 -0600

    add .pdf to filename

--- api/controllers/printPdfController.js
+++ api/controllers/printPdfController.js
@@ -27,25 +27,25 @@ const options = {
 exports.print_url = function(req, res) {
     var randomPrefixedTmpfile = uniqueFilename(options.dir);
     htmlPdf.create(req.query.url, options.htmlPDF).then((pdf) => pdf.toFile(randomPrefixedTmpfile));
-    res.json({url: req.query.url, pdf: path.basename(randomPrefixedTmpfile)});
+    res.json({url: req.query.url, pdf: path.basename(randomPrefixedTmpfile)+'.pdf'});
 };
 
 exports.print_html = function(req, res) {
     var randomPrefixedTmpfile = uniqueFilename(options.dir);
     htmlPdf.create(req.body.data, options.htmlPDF).then((pdf) => pdf.toFile(randomPrefixedTmpfile));
-    res.json({length: req.body.data.length, pdf: path.basename(randomPrefixedTmpfile)});
+    res.json({length: req.body.data.length, pdf: path.basename(randomPrefixedTmpfile)+'.pdf'});
 };
 
 exports.get_pdf = function(req,res) {
 	// Ensure no one tries a directory traversal
-	if(req.query.file.indexOf('..') !== -1) {
+	if(req.query.file.indexOf('..') !== -1 || req.query.file.indexOf('.pdf') == -1) {
 		res.status(400).send('Invalid filename!');
 		return;
 	}
 
-    res.setHeader('Content-disposition', 'attachment; filename=output.pdf');
+    res.setHeader('Content-disposition', 'attachment; filename='+req.query.file);
     res.setHeader('Content-type', 'application/pdf');
-    var filestream = fs.createReadStream(options.dir+'/'+req.query.file);
+    var filestream = fs.createReadStream(options.dir+'/'+req.query.file.replace('.pdf',''));
     filestream.pipe(res);
 };
 
