ba0f4108e01ddbbb612e39d914f882e2924cb2c8
Nathanael d. Noblet <nathanael@noblet.ca>
2017-07-21 11:07:51 -0600

    attempt to protect from requests for directory traversal files

--- api/controllers/printPdfController.js
+++ api/controllers/printPdfController.js
@@ -31,6 +31,12 @@ exports.print_html = function(req, res) {
 };
 
 exports.get_pdf = function(req,res) {
+	// Ensure no one tries a directory traversal
+	if(req.query.file.indexOf('..') !== -1) {
+		res.status(400).send('Invalid filename!');
+		return;
+	}
+
     res.setHeader('Content-disposition', 'attachment; filename=output.pdf');
     res.setHeader('Content-type', 'application/pdf');
     var filestream = fs.createReadStream('/tmp/'+req.query.file);
