9f02c61d299109d748a7f7e761a4a7b300b3b218
Nathanael d. Noblet <nathanael@noblet.ca>
2017-07-25 09:26:58 -0600

    configure if we use SSL via environment variables

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app = express();
 const options = {
 	port: process.env.PORT || 3000,
 	address: process.env.LISTEN || '127.0.0.1',
+	use_ssl: process.env.USE_SSL || true,
 	keyPath: process.env.SSL_KEY || 'privkey.pem',
 	certPath: process.env.SSL_CERT || 'cert.pem'
 };
@@ -18,10 +19,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const routes = require('./api/routes/printPdfRoutes');
 routes(app);
 
-console.log('KEY: '+options.keyPath+"\nCert: "+options.certPath+"\nPort: "+options.port);
-https.createServer({
-	key: fs.readFileSync(options.keyPath),
-	cert: fs.readFileSync(options.certPath)
-},app).listen(options.port,options.address);
+if(options.use_ssl === true) {
+    console.log('USING SSL! KEY: '+options.keyPath+"\nCert: "+options.certPath+"\nPort: "+options.port);
+    https.createServer({
+        key: fs.readFileSync(options.keyPath),
+        cert: fs.readFileSync(options.certPath)
+    },app).listen(options.port,options.address);
+} else {
+    console.log("**NOT** USING SSL! \nPort: "+options.port);
+	app.listen(options.port,options.address);
+}
 
 console.log('HTML to PDF RESTful API server started');
